28c376579a03d32bc054ceeca8e7038b08f72546
Abiodun Osagie <abiodunosagie8@gmail>
2025-01-13 13:22:30 +0100

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Abiodun Osagie on 13/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
