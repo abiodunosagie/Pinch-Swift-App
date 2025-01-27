00dadce906cc625d6cf9ca5e980b0a020d872aed
Abiodun Osagie <abiodunosagie8@gmail>
2025-01-27 23:23:50 +0100

    Complete update

---
+++ Pinch/Model/PageModel.swift
@@ -0,0 +1,8 @@
+//
+//  PageModel.swift
+//  Pinch
+//
+//  Created by Abiodun Osagie on 27/01/2025.
+//
+
+import Foundation
---
+++ Pinch/View/ControlImageView.swift
@@ -0,0 +1,18 @@
+//
+//  ControlImageView.swift
+//  Pinch
+//
+//  Created by Abiodun Osagie on 27/01/2025.
+//
+
+import SwiftUI
+
+struct ControlImageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ControlImageView()
+}
